47beb8e2adc8fb78ceaff85106d6e6823aae1518
David Su <david.d.su@gmail.com>
2016-02-02 16:02:45 +0100

    import kino library (works with window link but not actual .ts script files)

--- assets/Game (8)/script.ts
+++ assets/Game (8)/script.ts
@@ -1,3 +1,17 @@
+// link to ext libs
+
+declare const window;
+const timbreLink = window.document.createElement("script");
+// timbreLink.src = "https://cdn.rawgit.com/usdivad/kino/master/web/lib/timbre.js";
+timbreLink.src = "libraries/kino/lib/timbre.js"; // -> Superpowers/Contents/Resources/system/game/public/kino/lib/timbre.js
+window.document.body.appendChild(timbreLink);
+
+const kinoLink = window.document.createElement("script");
+// kinoLink.src = "https://cdn.rawgit.com/usdivad/kino/master/web/src/Kino.js";
+kinoLink.src = "libraries/kino/src/Kino.js";
+window.document.body.appendChild(kinoLink);
+
+
 // cannon
 let world = Sup.Cannon.getWorld();
 world.gravity.set(0, -100, 0);
