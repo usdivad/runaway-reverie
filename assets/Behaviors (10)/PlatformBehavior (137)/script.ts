e7ba55e103b4bfbfb87847e36713781254979003
David Su <david.d.su@gmail.com>
2016-11-09 20:10:25 +0100

    add opacity adjustment to platform behavior

--- assets/Behaviors (10)/PlatformBehavior (137)/script.ts
+++ assets/Behaviors (10)/PlatformBehavior (137)/script.ts
@@ -1,7 +1,7 @@
 class PlatformBehavior extends Sup.Behavior {
-  private originalCannonBody : Sup.Cannon.Body;
+  // private originalCannonBody : Sup.Cannon.Body;
   awake() {
-    this.originalCannonBody = this.actor.cannonBody;
+    // this.originalCannonBody = this.actor.cannonBody;
   }
 
   update() {
@@ -11,26 +11,32 @@ class PlatformBehavior extends Sup.Behavior {
     
     let playerIsDreaming = playerActor.getBehavior(CharacterBehavior).isDreaming;
     
+    let pos = this.actor.getPosition();
+    
     // opacity
-    if (playerY > this.actor.getPosition().y) {
-      Sup.log(playerY + " > " + this.actor.getPosition().y);
+    if (playerY > pos.y) { // above platform
+      // Sup.log(playerY + " > " + this.actor.getPosition().y);
+      this.actor.spriteRenderer.setOpacity(0.85);
+    }
+    else { // below platform
+      this.actor.spriteRenderer.setOpacity(0.45);
     }
     
     // cannonbody box for dreaming up
     if (playerIsDreaming) {
-      Sup.log("dreaming..");
+      // Sup.log("dreaming..");
       if (this.actor.cannonBody) {
-        // this.actor.cannonBody.destroy();
         this.actor.cannonBody.body.shapes.forEach(shape => {
           shape.collisionResponse = false;
         }); 
       }
     }
     else {
-      // this.actor.cannonBody = this.originalCannonBody;
-      this.actor.cannonBody.body.shapes.forEach(shape => {
-        shape.collisionResponse = true;
-      }); 
+      if (this.actor.cannonBody) {
+        this.actor.cannonBody.body.shapes.forEach(shape => {
+          shape.collisionResponse = true;
+        }); 
+      }
     }
   }
 }
