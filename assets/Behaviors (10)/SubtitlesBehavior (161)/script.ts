fc83afd552f92bbe220493660e10382a921c420e
David Su <david.d.su@gmail.com>
2016-11-12 23:41:37 +0100

    add scheduleText() method for time-based setting of subtitles

--- assets/Behaviors (10)/SubtitlesBehavior (161)/script.ts
+++ assets/Behaviors (10)/SubtitlesBehavior (161)/script.ts
@@ -1,8 +1,19 @@
 class SubtitlesBehavior extends Sup.Behavior {
   // private offset: Sup.Math.Vector3 = new Sup.Math.Vector3(0, -15, -8); // for cameraman
   private offset: Sup.Math.Vector3 = new Sup.Math.Vector3(0, 18, 0); // for player
+  private evtMul: number = 1000;
+  private conductor: Sup.Audio.Conductor;
 
   awake() {
+    // init params
+    this.conductor = new Sup.Audio.Conductor(120, 4, []);
+
+    // schedule text events
+    let t = 0;
+    t = this.scheduleText("Were I to wake from this \n runaway reverie...", t + 0);
+    t = this.scheduleText("... no unfractured radiance \n would shine from the stars.", t + 4);
+    t = this.scheduleText("", t + 4);
+    
     this.update();
   }
 
@@ -10,6 +21,15 @@ class SubtitlesBehavior extends Sup.Behavior {
     // let p = Sup.getActor("Cameraman").cannonBody.body.position;
     let p = Sup.getActor("Player").cannonBody.body.position;
     this.actor.setPosition(p.x + this.offset.x, p.y + this.offset.y, p.z + this.offset.z);
+  }
+
+  // schedule a setText() for this actor's text renderer
+  scheduleText(text, time) {
+    let subs = this;
+    this.conductor.scheduleEvent(time * this.evtMul, function() {
+      subs.actor.textRenderer.setText(text.replace(" \n ", "\n"));
+    });
+    return time;
   }
 }
 Sup.registerBehavior(SubtitlesBehavior);
